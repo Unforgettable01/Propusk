d4411787ce2c478ee9da4cf323cf6f13dca89360
Unforgettable01 <boryanich01@gmail.com>
2021-10-09 20:09:54 +0400

    Логика  + БД

--- Database/Implements/ClientStorage.cs
+++ Database/Implements/ClientStorage.cs
@@ -1,10 +1,92 @@
-﻿using System;
+﻿using Database.Models;
+using Logic.BindingModels;
+using Logic.Interfaces;
+using Logic.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Database.Implements
 {
-    class ClientStorage
+    public class ClientStorage : IClientStorage
     {
+        public List<ClientViewModel> GetFullList()
+        {
+            using (var context = new DatabaseContext())
+            {
+                return context.Clients
+                .Select(CreateModel).ToList();
+            }
+        }
+
+        public List<ClientViewModel> GetFilteredList(ClientBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            using (var context = new DatabaseContext())
+            {
+                return context.Clients
+                    .Where(rec => rec.ClientFIO.Contains(model.ClientFIO) || (rec.ClientFIO.Equals(model.ClientFIO)))
+                    .Select(CreateModel).ToList();
+            }
+        }
+
+        public ClientViewModel GetElement(ClientBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            using (var context = new DatabaseContext())
+            {
+                var client = context.Clients
+                    .FirstOrDefault(rec => rec.ClientFIO.Equals(model.ClientFIO) || rec.id == model.Id);
+                return client != null ? CreateModel(client) : null;
+            }
+        }
+
+        public void Insert(ClientBindingModel model)
+        {
+            using (var context = new DatabaseContext())
+            {
+                context.Clients.Add(CreateModel(model, new Client()));
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(ClientBindingModel model)
+        {
+            using (var context = new DatabaseContext())
+            {
+                Client element = context.Clients.FirstOrDefault(rec => rec.id == model.Id);
+                if (element != null)
+                {
+                    context.Clients.Remove(element);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Клиент не найден");
+                }
+            }
+        }
+
+        private Client CreateModel(ClientBindingModel model, Client client)
+        {
+            client.ClientFIO = model.ClientFIO;            
+            return client;
+        }
+
+        private ClientViewModel CreateModel(Client client)
+        {
+            return new ClientViewModel
+            {
+                Id = client.id,
+                ClientFIO = client.ClientFIO
+            };
+        }
     }
 }
--- Database/Implements/WorkerStorage.cs
+++ Database/Implements/WorkerStorage.cs
@@ -1,10 +1,95 @@
-﻿using System;
+﻿using Database.Models;
+using Logic.BindingModels;
+using Logic.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Database.Implements
 {
-    class WorkerStorage
+    public class WorkerStorage
     {
+        public List<WorkerViewModel> GetFullList()
+        {
+            using (var context = new DatabaseContext())
+            {
+                return context.Workers
+                .Select(CreateModel).ToList();
+            }
+        }
+
+        public List<WorkerViewModel> GetFilteredList(WorkerBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            using (var context = new DatabaseContext())
+            {
+                return context.Workers
+                    .Where(rec => rec.WorkerFIO.Contains(model.WorkerFIO) || (rec.WorkerFIO.Equals(model.WorkerFIO)))
+                    .Select(CreateModel).ToList();
+            }
+        }
+
+        public WorkerViewModel GetElement(WorkerBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            using (var context = new DatabaseContext())
+            {
+                var worker = context.Workers
+                    .FirstOrDefault(rec => rec.WorkerFIO.Equals(model.WorkerFIO) || rec.id == model.Id);
+                return worker != null ? CreateModel(worker) : null;
+            }
+        }
+
+        public void Insert(WorkerBindingModel model)
+        {
+            using (var context = new DatabaseContext())
+            {
+                context.Workers.Add(CreateModel(model, new Worker()));
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(WorkerBindingModel model)
+        {
+            using (var context = new DatabaseContext())
+            {
+                Worker element = context.Workers.FirstOrDefault(rec => rec.id == model.Id);
+                if (element != null)
+                {
+                    context.Workers.Remove(element);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Работник не найден");
+                }
+            }
+        }
+
+        private Worker CreateModel(WorkerBindingModel model, Worker worker)
+        {
+            worker.WorkerFIO = model.WorkerFIO;
+            worker.WorkingTime = model.WorkingTime;
+            worker.PauseTime = model.PauseTime;
+            return worker;
+        }
+
+        private WorkerViewModel CreateModel(Worker worker)
+        {
+            return new WorkerViewModel
+            {
+                Id = worker.id,
+                WorkerFIO = worker.WorkerFIO,
+                WorkingTime = worker.WorkingTime,
+                PauseTime = worker.PauseTime
+            };
+        }
     }
 }
